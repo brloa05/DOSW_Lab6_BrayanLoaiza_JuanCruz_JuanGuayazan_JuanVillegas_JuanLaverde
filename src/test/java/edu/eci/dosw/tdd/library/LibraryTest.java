c2d54ab3165bc0508a2f8aad455d5f52da5b71b8
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2026-03-09 23:03:07 -0500

    Add unit tests for addBook, loanABook, and returnLoan methods in LibraryTest

--- src/test/java/edu/eci/dosw/tdd/library/LibraryTest.java
+++ src/test/java/edu/eci/dosw/tdd/library/LibraryTest.java
@@ -6,4 +6,90 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LibraryTest {
 
+    @BeforeEach
+    public void setUp() {
+        // Configuración inicial
+    }
+
+    // ========== PRUEBAS PARA addBook ==========
+
+    @Test
+    public void testAddBook1() {
+        // TODO: Implementar caso de prueba 1
+    }
+
+    @Test
+    public void testAddBook2() {
+        // TODO: Implementar caso de prueba 2
+    }
+
+    @Test
+    public void testAddBook3() {
+        // TODO: Implementar caso de prueba 3
+    }
+
+    @Test
+    public void testAddBook4() {
+        // TODO: Implementar caso de prueba 4
+    }
+
+    @Test
+    public void testAddBook5() {
+        // TODO: Implementar caso de prueba 5
+    }
+
+    // ========== PRUEBAS PARA loanABook ==========
+
+    @Test
+    public void testLoanABook1() {
+        // TODO: Implementar caso de prueba 1
+    }
+
+    @Test
+    public void testLoanABook2() {
+        // TODO: Implementar caso de prueba 2
+    }
+
+    @Test
+    public void testLoanABook3() {
+        // TODO: Implementar caso de prueba 3
+    }
+
+    @Test
+    public void testLoanABook4() {
+        // TODO: Implementar caso de prueba 4
+    }
+
+    @Test
+    public void testLoanABook5() {
+        // TODO: Implementar caso de prueba 5
+    }
+
+    // ========== PRUEBAS PARA returnLoan ==========
+
+    @Test
+    public void testReturnLoan1() {
+        // TODO: Implementar caso de prueba 1
+    }
+
+    @Test
+    public void testReturnLoan2() {
+        // TODO: Implementar caso de prueba 2
+    }
+
+    @Test
+    public void testReturnLoan3() {
+        // TODO: Implementar caso de prueba 3
+    }
+
+    @Test
+    public void testReturnLoan4() {
+        // TODO: Implementar caso de prueba 4
+    }
+
+    @Test
+    public void testReturnLoan5() {
+        // TODO: Implementar caso de prueba 5
+    }
+
 }

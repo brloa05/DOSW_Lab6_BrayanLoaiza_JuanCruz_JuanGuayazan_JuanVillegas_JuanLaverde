b77e748c141bb029266a13a24a9a474c3ad5da78
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2026-03-09 22:49:21 -0500

    Add initial LibraryTest class with setup for unit tests

---
+++ src/test/java/edu/eci/dosw/tdd/library/LibraryTest.java
@@ -0,0 +1,9 @@
+package edu.eci.dosw.tdd.library;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LibraryTest {
+
+}
